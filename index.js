d41db8fc5a11e15a9c51dc900e1df922fec3297c
ujangaja <ujangaja@gmail.com>
2019-12-11 06:18:37 +0700

    install and setup mongoose to connect to db

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
 
+// Connect to db
+mongoose.connect('mongodb://localhost/miniecomm')
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Connect to mongodeb!');
+});
+//view engin setup
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World')
 // })
+
+
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+//Start the server
 app.listen(port, () => {
     console.log(`Listening Server on prot ${port}`);
 })
