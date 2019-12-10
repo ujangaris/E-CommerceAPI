4efb4f9de2ea580cf9f439de151ed80479370717
ujangaja <ujangaja@gmail.com>
2019-12-10 15:19:11 +0700

    update data package ejs, file views/index.js

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
+// app.get('/', (req, res) => {
+//     res.send('Hello World')
+// })
+app.get('/', (req, res) => {
+    res.render('index')
+})
 app.listen(port, () => {
     console.log(`Listening Server on prot ${port}`);
 })
