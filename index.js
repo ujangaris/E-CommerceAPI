1be684c79c6267a0d600aef0a36d075520e87d02
ujangaja <ujangaja@gmail.com>
2019-12-12 07:54:57 +0700

    settup message validator

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ app.use(app.use(session({
     }
 }))
 
+//Express message validator
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
 
 
 // app.get('/', (req, res) => {
