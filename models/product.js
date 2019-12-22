2831497b875fa586f9707fe1c86cc075e166293d
ujangaja <ujangaja@gmail.com>
2019-12-22 23:28:09 +0700

    Schema product

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const monggose = require('mongoose')
+
+//Product Schema
+var Product Schema = monggose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String
+    },
+})
+
+
+var Product = module.exports = monggose.model('Product', ProductSchema)
